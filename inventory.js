88d6b8020470c3f4d6fda0135cd2250364166b85
CharlyLuna <cluna10@ucol.mx>
2021-10-15 22:31:37 -0500

    base files

---
+++ inventory.js
@@ -0,0 +1,86 @@
+export default class Inventory {
+  constructor() {
+    this._products = new Array();
+  }
+
+  add(product) {
+    if (this.search(product.getCode()) == null && this._products.length <= 20) {
+      this._products.push(product);
+      return true;
+    }
+    return false;
+  }
+
+  search(code) {
+    let result = null;
+    this._products.forEach((product) => {
+      if (code == product.getCode()) {
+        result = product;
+      }
+    });
+    return result;
+  }
+
+  list() {
+    let i;
+    let message = "";
+    for (i = 0; i < this._products.length; i++) {
+      message += `<div>
+        ${this._products[i].getCode()} - ${this._products[i].getName()}  
+        <div>`;
+    }
+    return message;
+  }
+
+  inverseList() {
+    let i = this._products.length - 1;
+    let message = "";
+    for (; i >= 0; i--) {
+      message += `<div>
+        ${this._products[i].getCode()} - ${this._products[i].getName()}   
+        <div>`;
+    }
+    return message;
+  }
+
+  insert(product, position) {
+    if (position && position <= this._products.length && this.add(product)) {
+      let i = this._products.length - 1;
+      let j = i - 1;
+      for (; i >= position; i--, j--) {
+        let value = this._products[i];
+        this._products[i] = this._products[j];
+        this._products[j] = value;
+      }
+      return this._products;
+    }
+    return false;
+  }
+
+  delete(code) {
+    // buscamos primero si esta el producto
+    if (this.search(code) == null) {
+      return null;
+    }
+    let pos = this._productPos(code);
+    let i = pos;
+    let j = i + 1;
+    //movemos de posicion al producto hasta el final del array;
+    for (; j < this._products.length; i++, j++) {
+      let value = this._products[i];
+      this._products[i] = this._products[j];
+      this._products[j] = value;
+    }
+    return this._products.pop();
+  }
+
+  _productPos(code) {
+    let pos;
+    this._products.forEach((p, index) => {
+      if (code == p.getCode()) {
+        pos = index;
+      }
+    });
+    return pos;
+  }
+}
---
+++ product.js
@@ -0,0 +1,33 @@
+export default class Product {
+  constructor(code, name, quantity, cost) {
+    this._code = code;
+    this._name = name.toUpperCase();
+    this._quantity = quantity;
+    this._cost = cost;
+    this._position;
+  }
+
+  getName() {
+    return this._name;
+  }
+  getCode() {
+    return this._code;
+  }
+
+  getValue() {
+    return this._quantity * this._cost;
+  }
+  infoHtml() {
+    return `
+      <div>
+        <h3>
+          ${this._code}-${this._name}
+        </h3>
+        <p>
+      ${this._quantity} PIEZA(s) COSTO:${this._cost}$ 
+        VALOR DE MERCANCIA: ${this.getValue()}$
+      </p>
+      </div>
+      `;
+  }
+}
